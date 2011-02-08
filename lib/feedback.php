bd4dd0b14c58c9a93f376f3be4f7fa80c2a3a245
Jeff Tilson <jrtilson@gmail.com>
2011-02-08 23:31:06 +0000

    Forgot to add new lib file. Oops.

---
+++ lib/feedback.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Feedback helper functions
+ * 
+ * @package Feedback
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @author Jeff Tilson
+ * @copyright THINK Global School 2010
+ * @link http://www.thinkglobalschool.com/
+ * 
+ */
+
+/** 
+* Helper function to get a list of available status'
+*
+* @return array
+*/
+function get_status_types() {
+	return array(
+		FEEDBACK_STATUS_SUBMITTED => elgg_echo("submitted"), 
+	 	FEEDBACK_STATUS_ACKNOWLEDGED => elgg_echo("acknowledged"), 
+	 	FEEDBACK_STATUS_RESOLVED => elgg_echo("resolved"),
+	 	FEEDBACK_STATUS_INPROGRESS => elgg_echo("inprogress"), 
+	 	FEEDBACK_STATUS_DUPLICATE => elgg_echo("duplicate")
+	);
+}
